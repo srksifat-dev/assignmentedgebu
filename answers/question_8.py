72c0f13778cb599d1ebd55840952b35bd59a629e
mshoibur19.glm <mshoibur19.glm@bu.ac.bd>
2024-10-07 09:37:28 +0000

    question 8 is solved

---
+++ answers/question_8.py
@@ -0,0 +1,13 @@
+# Suggested code may be subject to a license. Learn more: ~LicenseLog:1479734646.
+class Book:
+    def __init__(self, title, author, pages):
+        self.title = title
+        self.author = author
+        self.pages = pages
+    
+    def get_description(self):
+        return f"{self.title} by {self.author}, Pages: {self.pages}"
+
+
+book1 = Book("The Alchemist", "Paulo Coelho", 150)
+print(book1.get_description())
