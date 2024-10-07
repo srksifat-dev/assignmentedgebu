7b1894f3a6e16ab612afdaa7fe62f87c822bca34
mshoibur19.glm <mshoibur19.glm@bu.ac.bd>
2024-10-07 08:25:28 +0000

    question 3 is solved

---
+++ answers/question_3.py
@@ -0,0 +1,9 @@
+def digitCounter(s):
+    digits = 0
+    for char in s:
+        if char.isdigit():
+            digits += 1
+    return digits
+
+input = input("Enter a random string: ")
+print(f"Total digits are : {digitCounter(input)}")
